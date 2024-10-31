4e41ef7436579418e4e11069060ca0475e238ec7
Israel <israel.asefa.mi1055@gmail.com>
2024-10-31 18:09:54 +0300

    chappa test integration for paymnets

--- src/app/(app)/api/handlePayment/paymentCallback/route.ts
+++ src/app/(app)/api/handlePayment/paymentCallback/route.ts
@@ -1,11 +1,6 @@
 import { NextResponse } from "next/server"
 // import {crypto} from 'crypto'
 export async function POST(req: Request) {
-    const hash = crypto
-    .("sha256", process.env.CHAPPA_SECRET)
-    .update(JSON.stringify(req.body))
-    .digest("hex");
-    const payload= await req.json()
-    console.log(payload)
+
     return NextResponse.json({status:200})
 }
